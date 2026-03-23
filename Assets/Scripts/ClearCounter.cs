95390b6df3e9d9e8b0a9866e8bd1b86eb292d391
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2026-03-23 14:11:29 -0700

    player_pickup_progress

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -16,10 +16,24 @@ public class ClearCounter : BaseCounter
 			    // Player is carrying something
 			    player.GetKitchenObject().SetKitchenObjectParent(this);
 		    }
+		    else
+		    {
+			    // Player not carrying anything
+		    }
 	    }
 	    else
 	    {
 		    // There is a KitchenObject here
+		    if (player.HasKitchenObject())
+		    {
+			    // Player is carrying something
+			    
+		    }
+		    else
+		    {
+			    // Player is not carrying anything
+			    GetKitchenObject().SetKitchenObjectParent(player);
+		    }
 	    }
     }
 }
