c4898cb7f3dac83403d1d78fa1ffa92d51ca95c4
MohammadShahroudi <149831372+MohammadShahroudi@users.noreply.github.com>
2026-04-03 17:06:48 -0700

    surpassed_7hours

---
+++ Assets/Scripts/PlateIconsSingleUI.cs
@@ -0,0 +1,13 @@
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlateIconsSingleUI : MonoBehaviour
+{
+
+    [SerializeField] private Image image;
+    
+    public void SetKitchenObjectSO(KitchenObjectSO kitchenObjectSO)
+    {
+        image.sprite = kitchenObjectSO.sprite;
+    }
+}
